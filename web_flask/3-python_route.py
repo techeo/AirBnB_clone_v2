7394f7d44e5fd5cdbb210894989e4897e6e1c3b4
techeo <tegaecheo@gmail.com>
2024-05-17 03:50:47 -0700

    python route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+  i  return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello_c(text):
+    return 'C %s' % text.replace("_", " ")
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def hello_python(text='is cool'):
+    return 'Python {}'.format(text.replace("_", " ")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
