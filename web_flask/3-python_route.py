25dc564bfef85f97247a541cd3bcb3bb7eb9122f
techeo <tegaecheo@gmail.com>
2024-05-17 04:00:40 -0700

    changes made

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,7 +21,7 @@ def hello_c(text):
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def hello_python(text='is cool'):
+def hello_python(text="is cool"):
     return 'Python {}'.format(text.replace("_", " "))
 
 
