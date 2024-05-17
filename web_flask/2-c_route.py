b46bafbcc5f424a063e78074fdad7cb7e6b4f347
techeo <tegaecheo@gmail.com>
2024-05-17 03:04:59 -0700

    changes

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -10,13 +10,16 @@ app = Flask(__name__)
 def hello():
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes = False)
 def hbnb():
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes = False)
 def c(text):
     return 'C {}'.format(text.replace("_"," "))
+
 
 if __name__ == '__main__':
         app.run(host = '0.0.0.0', port = 5000)
