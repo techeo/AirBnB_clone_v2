612cc782cd8d17c0636d8fad7aa5daaf076135b5
techeo <tegaecheo@gmail.com>
2024-05-17 03:10:42 -0700

    changes made

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""A script that starts a flask appication:
-    listening on 0.0.0.0, port 5000
-    with three routes"""
+"""A script that starts a flask appication."""
 
 from flask import Flask
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
