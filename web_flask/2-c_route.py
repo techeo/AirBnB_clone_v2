8553e1e3e57f46d34339f55d440b9eeca85cd679
techeo <tegaecheo@gmail.com>
2024-05-17 02:46:04 -0700

    2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A script that starts a flask appication:
+    listening on 0.0.0.0, port 5000
+    with three routes"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes = False)
+def hello():
+        return "Hello HBNB!"
+@app.route('/hbnb',strict_slashes = False)
+def hbnb():
+        return "HBNB"
+@app.route('/c/<text>',strict_slashes = False)
+def c(text):
+        return "C {}".format(text.replace("_"," "))
+
+if __name__ == '__main__':
+        app.run(host = '0.0.0.0', port = 5000)
