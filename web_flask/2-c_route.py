d8184b00584a18a73700650111da43befd33f397
techeo <tegaecheo@gmail.com>
2024-05-17 03:08:01 -0700

    adjustments

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,20 +6,20 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/', strict_slashes = False)
+@app.route('/', strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes = False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes = False)
+@app.route('/c/<text>', strict_slashes=False)
 def c(text):
     return 'C {}'.format(text.replace("_"," "))
 
 
 if __name__ == '__main__':
-        app.run(host = '0.0.0.0', port = 5000)
+    app.run(host = '0.0.0.0', port = 5000)
